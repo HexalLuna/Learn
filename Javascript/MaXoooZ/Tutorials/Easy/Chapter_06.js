df760e436c386e5133e57b73a4ea250c7e0443b8
MaXoooZ <55662431+Max-xoo@users.noreply.github.com>
2020-12-06 08:48:36 +0100

    Chapter 6 & Practice 1 !

--- Javascript/MaXoooZ/Tutorials/Easy/Chapter_06.js
+++ Javascript/MaXoooZ/Tutorials/Easy/Chapter_06.js
@@ -1,1 +1,36 @@
-// Soon will be added chapters, for now just that and the documentation!
+// Chapter 6
+
+// FIRST PRACTICE!!
+function fr() {
+    /* 
+    	Bonjour, bienvenue sur mon avancée sur le javascript (que j'ai déjà appris entièrement).
+    	Bonne apprentissage à vous et j'espère vous revoir dev fullstack :').
+
+    	Bon avant de pouvoir executer du code, je vous propose un site pour pouvoir executer vos lignes de code !
+    	https://jsbin.com/juwokohedi/edit?js,console
+    
+    	(Tout est retrouvable dans le README.md)
+    */
+
+    /*
+     	Alors, dans ce chapitre vous allez avoir un petit exercice ! Votre premier pour mettre en place vos débuts de connaissances !
+     	Veuillez vous rendre dans /Javascript/MaXoooZ/Practice/Practice_01.js
+    */
+}
+
+function en() {
+    /* 
+    	Hello, welcome to my progress on javascript (which I have already learned completely).
+    	Happy learning to you and I hope to see you again dev fullstack: ').
+
+    	Good before being able to execute code, I suggest you a site to be able to execute your lines of code!
+    	https://jsbin.com/juwokohedi/edit?js,console
+    
+    	(Everything can be found in the README.md)
+    */
+
+	/*
+      	So, in this chapter you will have a little exercise! Your first to set up your knowledge beginnings!
+      	Please go to /Javascript/MaXoooZ/Practice/Practice_01.js
+    */
+}
