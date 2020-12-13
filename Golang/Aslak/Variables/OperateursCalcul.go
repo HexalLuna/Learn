4e5d1caceff87cc0caf91dee9e95f6177ecbc7ba
AslakOffi <51925794+AslakOffi@users.noreply.github.com>
2020-12-13 18:41:18 +0100

    Create OperateursCalcul.go

---
+++ Golang/Aslak/Variables/OperateursCalcul.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+    var a int = 4
+    var b int = 2
+
+    fmt.Println("a + b  = ", a+b) // addition de la variable a et b
+    fmt.Println("a - b  = ", a-b) // soustraction de la variable a et b
+    fmt.Println("a * b  = ", a*b) // multiplication de la variable a et b
+    fmt.Println("a / b  = ", a/b) // division de la variable a et b
+    fmt.Println("a % b  = ", a%b) // modulo de la variable a et b
+}
+
+/* Résultat :
+
+a + b  =  6
+a - b  =  2
+a * b  =  8
+a / b  =  2
+a % b  =  0
+*/
