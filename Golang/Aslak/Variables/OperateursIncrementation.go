6ed857853c6da6f9b9f9284e9a847d51ca7502bf
AslakOffi <51925794+AslakOffi@users.noreply.github.com>
2020-12-13 18:44:42 +0100

    Create OperateursIncrementation.go

---
+++ Golang/Aslak/Variables/OperateursIncrementation.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+    var a int = 4
+
+    a ++ // incrémentation
+    fmt.Println("incrémentation de 1 : ", a)
+
+    a -- // décrémentation
+    fmt.Println("décrémentation de 1 : ", a)
+}
+
+/* Résultat :
+
+incrémentation de 1 :  5
+décrémentation de 1 :  4
+
+*/
