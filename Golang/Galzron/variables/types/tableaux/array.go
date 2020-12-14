3e862e3e18d565079ae770dc2835e631b336d173
AlexOZx <69150061+Galzron@users.noreply.github.com>
2020-12-14 10:20:16 +0100

    Update array.go

--- Golang/Galzron/variables/types/tableaux/array.go
+++ Golang/Galzron/variables/types/tableaux/array.go
@@ -3,11 +3,11 @@ package main
 import "fmt"
 
 func main()  {
-	var array [5]int //mon tableau va prendre 5 éléments
+    var array [5]int //mon tableau va prendre 5 éléments
 
-	for i := 0; i < 5; i++ {
-		array[i] = i + 1 //ici je place des valeurs dans mon array
-	}
+    for i := 0; i < 5; i++ {
+    	array[i] = i + 1 //ici je place des valeurs dans mon array
+    }
 
-	fmt.Printf("%v", array)
+    fmt.Printf("%v", array)
 }
