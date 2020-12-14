3fbfd1fb581f57511ac04013ad17c5ea2ede5009
AlexOZx <69150061+Galzron@users.noreply.github.com>
2020-12-14 10:22:26 +0100

    Update incrementation.go

--- Golang/Galzron/variables/operators/incrementation.go
+++ Golang/Galzron/variables/operators/incrementation.go
@@ -3,18 +3,18 @@ package main
 import "fmt"
 
 func main()  {
-	var A int = 6
+    var A int = 6
 
-	A++ //Ceci est une incrémentation, je rajoute 1 à la valeur de ma variable
-	fmt.Println("Vous venez d'incrémenter de 1 la valeur de la variable A, sa valeur est désormais de: ", A)
+    A++ //Ceci est une incrémentation, je rajoute 1 à la valeur de ma variable
+    fmt.Println("Vous venez d'incrémenter de 1 la valeur de la variable A, sa valeur est désormais de: ", A)
 
-	A-- //Ceci est une décrémentation, je soustrais 1 à la valeur de ma variable
-	fmt.Println("Vous venez de décrémenter de 1 la valeur de la variable A, sa valeur est désormais de: ", A)
+    A-- //Ceci est une décrémentation, je soustrais 1 à la valeur de ma variable
+    fmt.Println("Vous venez de décrémenter de 1 la valeur de la variable A, sa valeur est désormais de: ", A)
 
-	/**
-	 * Résultats:
-	 *
-	 * Incrémenration, A = 7
-	 * Décrémentation, A = 5
-	**/
+    /**
+     * Résultats:
+     *
+     * Incrémenration, A = 7
+     * Décrémentation, A = 5
+    **/
 }
