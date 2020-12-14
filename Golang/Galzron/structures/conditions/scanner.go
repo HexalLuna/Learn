936222b0c5c2df3b6fe6bd3decc923e74c66eb6f
AlexOZx <69150061+Galzron@users.noreply.github.com>
2020-12-14 11:20:27 +0100

    Update scanner.go

--- Golang/Galzron/structures/conditions/scanner.go
+++ Golang/Galzron/structures/conditions/scanner.go
@@ -13,3 +13,10 @@ func main() {
     entreeUtilisateur := bufio.NewScanner(os.Stdin).Text() // stockage du résultat du scanner dans une variable
     fmt.Println(entreeUtilisateur)
 }
+/**
+ * bufio.NewScanner() encapsule un objet io.Reader ou io.Writer, créant un autre objet (Reader ou Writer) qui implémente également l'interface
+ * en d'autres termes, tout ce qu'il fait, c'est ajouter une couche par exemple dans os.Stdin, il n'effectue aucune analyse ou interprétation du flux.
+ *
+ * En revanche, fmt.Scanln() lit les données à partir d'un flux (qui peut ou non être mis en mémoire tampon) 
+ * c'est-à-dire renvoyé par le package bufio, en divisant l'entrée par un espace, pour la stocker dans une slice.
+**/ 
