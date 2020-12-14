0fc1cceb2e9ab0165f511b6ffacb85100679e8ff
AlexOZx <69150061+Galzron@users.noreply.github.com>
2020-12-14 09:39:46 +0100

    Update elseIf.go

--- Golang/Galzron/structures/conditions/elseIf.go
+++ Golang/Galzron/structures/conditions/elseIf.go
@@ -1,7 +1,24 @@
 package main 
 
-import "fmt" 
+import "fmt"
 
-func main()  {
-	//TODO
+func main() {
+    var name string
+    var mdp int
+	
+    fmt.Println("Bonjour, veuillez vous identifiez par votre prénom s'il vous plaît:")
+    fmt.Scanln(&name)
+
+    fmt.Println("Mot de passe: ")
+    fmt.Scanln(&mdp)
+
+    if mdp == 24112004 && name == "Alexis" {
+	fmt.Println("Bienvenue Alexis !")
+
+    } else if mdp == 123456789 && name == "Clément" {
+	fmt.Println("Bienvenue Clément !")
+
+    } else {
+	fmt.Println("Vous n'êtes pas identifiable dans notre 'db'")
+    }	
 }
