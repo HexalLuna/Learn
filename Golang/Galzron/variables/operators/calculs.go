5fa947f15519f5a539449dec5c71f27676939ddf
AlexOZx <69150061+Galzron@users.noreply.github.com>
2020-12-14 10:21:29 +0100

    Update calculs.go

--- Golang/Galzron/variables/operators/calculs.go
+++ Golang/Galzron/variables/operators/calculs.go
@@ -4,29 +4,29 @@ import "fmt"
 
 func main()  {
 
-	var a int = 23
-	var b int = 5
+    var a int = 23
+    var b int = 5
 
-	c := a + b //les valeurs de a et b s'additionne dans une nouvelle variable appelée c
-	a += b //La valeur de b s'assigne a celle de la variable a
-	fmt.Println("a + b = ", c, "and a += b = ", a)
+    c := a + b //les valeurs de a et b s'additionne dans une nouvelle variable appelée c
+    a += b //La valeur de b s'assigne a celle de la variable a
+    fmt.Println("a + b = ", c, "and a += b = ", a)
 
-	//Nous allons maintenant réitérer cette opération mais avec les autres signes opératoires existants et en lister d'autres
-	d := a - b
-	a -= b
-	fmt.Println("a - b = ", d, "and a -= b = ", a)
+    //Nous allons maintenant réitérer cette opération mais avec les autres signes opératoires existants et en lister d'autres
+    d := a - b
+    a -= b
+    fmt.Println("a - b = ", d, "and a -= b = ", a)
 
-	e := a * b
-	a *= b
-	fmt.Println("a * b = ", e, "and a *= b = ", a)
+    e := a * b
+    a *= b
+    fmt.Println("a * b = ", e, "and a *= b = ", a)
 
-	f := a / b
-	a /= b
-	fmt.Println("a / b = ", f, "and a /= b = ", a)
+    f := a / b
+    a /= b
+    fmt.Println("a / b = ", f, "and a /= b = ", a)
 
-	g := a % b
-	a %= b
-	fmt.Println("a % b = ", g, "and a %= b = ", a)
+    g := a % b
+    a %= b
+    fmt.Println("a % b = ", g, "and a %= b = ", a)
 }
 
 /**
