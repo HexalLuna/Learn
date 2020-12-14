039e02fc038ea624b8deac82aaf3e1102451d4bd
AlexOZx <69150061+Galzron@users.noreply.github.com>
2020-12-14 09:57:08 +0100

    Update elseIf.go

--- Golang/Galzron/structures/conditions/elseIf.go
+++ Golang/Galzron/structures/conditions/elseIf.go
@@ -3,22 +3,22 @@ package main
 import "fmt"
 
 func main() {
-    var name string
-    var mdp int
+    var name string //déclaration d'une variable déstinée à contenir une chaîne de caractères
+    var mdp int //déclaration de la variable servant au mot de passe
 	
     fmt.Println("Bonjour, veuillez vous identifiez par votre prénom s'il vous plaît:")
-    fmt.Scanln(&name)
+    fmt.Scanln(&name) //scand ma variable name et ouvre une entrée utilisateur dans la console
 
     fmt.Println("Mot de passe: ")
-    fmt.Scanln(&mdp)
+    fmt.Scanln(&mdp) //scan ma variable mdp et ouvre une entrée utilisateur dans la console
 
-    if mdp == 24112004 && name == "Alexis" {
-	fmt.Println("Bienvenue Alexis !")
+    if mdp == 24112004 && name == "Alexis" { //vérification des variables
+	fmt.Println("Bienvenue Alexis !") //informations ok
 
-    } else if mdp == 123456789 && name == "Clément" {
-	fmt.Println("Bienvenue Clément !")
+    } else if mdp == 123456789 && name == "Clément" { //vérification des variables
+	fmt.Println("Bienvenue Clément !") //informations ok
 
     } else {
-	fmt.Println("Vous n'êtes pas identifiable dans notre 'db'")
+	fmt.Println("Vous n'êtes pas identifiable dans notre 'db'") //informations éronnées
     }	
 }
