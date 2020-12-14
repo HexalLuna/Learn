d8cfa3151d42847d11a73969996685ab3e35e615
AlexOZx <69150061+Galzron@users.noreply.github.com>
2020-12-14 10:19:41 +0100

    Update slice.go

--- Golang/Galzron/variables/types/tableaux/slice.go
+++ Golang/Galzron/variables/types/tableaux/slice.go
@@ -2,17 +2,17 @@ package main
 
 import "fmt"
 
-func min()  {
-	var slice []int //ceci est un slice de int, en quelque sorte une array sans limite de valeur définit
-	//var array []int{1, 2, 3} ==> autre manière de définir un slice
+func main()  {
+    var slice []int //ceci est un slice de int, en quelque sorte une array sans limite de valeur définit
+    //var array []int{1, 2, 3} ==> autre manière de définir un slice
 
-	for s := 0; s < 9; s++ {
-		slice = append(slice, s + 1) //ici je place des valeurs dans mon slice
-	}
+    for s := 0; s < 9; s++ {
+    	slice = append(slice, s + 1) //ici je place des valeurs dans mon slice
+    }
+	
+    for _, x := range slice {
+    	fmt.Println(x)
+    }
 
-	for _, x := range slice {
-		fmt.Println(x)
-	}
-
-	fmt.Printf("%v", slice)
+    fmt.Printf("%v", slice)
 }
